aa75c6d48f50f964ce329093acfa9c2e6d50fda5
Atviras Vilnius <atviras@vilnius.lt>
2016-05-20 22:40:10 +0300

    automatic update

---
+++ data/iq/src/Paysera/WalletApi.php
@@ -0,0 +1,193 @@
+<?php
+
+
+/**
+ * WalletApi
+ *
+ * @author Marius Balčytis <m.balcytis@evp.lt>
+ */
+class Paysera_WalletApi
+{
+    /**
+     * @var string
+     */
+    protected $clientId;
+
+    /**
+     * @var Paysera_WalletApi_Auth_SignerInterface
+     */
+    protected $signer;
+
+    /**
+     * @var Paysera_WalletApi_Util_Router
+     */
+    protected $router;
+
+    /**
+     * @var Paysera_WalletApi_Container
+     */
+    protected $container;
+
+    /**
+     * Constructor for entry point to client library.
+     *
+     * Credentials are required.
+     * Router can be configured and passed if some other endpoints are used, different from default ones.
+     * Container can be passed if some services are overriden with custom ones or if some listeners are bound to
+     * event dispatcher.
+     *
+     * @param string                                          $clientId       client ID
+     * @param string|Paysera_WalletApi_Http_ClientCertificate $authentication mac secret or certificate information
+     * @param Paysera_WalletApi_Util_Router                   $router         default is used if not passed
+     * @param Paysera_WalletApi_Container                     $container      default is used if not passed
+     */
+    public function __construct(
+        $clientId,
+        $authentication,
+        Paysera_WalletApi_Util_Router $router = null,
+        Paysera_WalletApi_Container $container = null
+    ) {
+        if ($router === null) {
+            $router = new Paysera_WalletApi_Util_Router();
+        }
+        if ($container === null) {
+            $container = new Paysera_WalletApi_Container();
+        }
+
+        $this->clientId = $clientId;
+        $this->signer = $container->createAuthSigner($clientId, $authentication);
+        $this->router = $router;
+        $this->container = $container;
+    }
+
+    /**
+     * Creates wallet client, responsible for creating transactions, getting user information etc.
+     *
+     * @param array $parameters project_id, location_id or some other parameters, if needed
+     *
+     * @return Paysera_WalletApi_Client_WalletClient
+     */
+    public function walletClient(array $parameters = array())
+    {
+        return $this->container->createWalletClient(
+            $this->basicClient($this->router->getWalletApiEndpoint(), null, $parameters)
+        );
+    }
+
+    /**
+     * Creates wallet client, related to specific access token. Requests are signed with given access token.
+     * Some specific methods are also available, which are only available when using access token.
+     *
+     * @param Paysera_WalletApi_Entity_MacAccessToken $token
+     * @param array                                   $parameters project_id, location_id or some other parameters
+     *
+     * @return Paysera_WalletApi_Client_TokenRelatedWalletClient
+     */
+    public function walletClientWithToken(
+        Paysera_WalletApi_Entity_MacAccessToken $token,
+        array $parameters = array()
+    ) {
+        $dispatcher = $this->dispatcher($this->router->getWalletApiEndpoint(), $token, $parameters);
+        $client = $this->container->createWalletClientWithToken($this->container->createBasicClient($dispatcher));
+        $client->setCurrentAccessToken($token);
+        $dispatcher->addSubscriber(new Paysera_WalletApi_Listener_AccessTokenSetter($client));
+        return $client;
+    }
+
+    /**
+     * Creates OAuth consumer, responsible for exchanging code to access token, getting code from parameters, getting
+     * redirect uri to OAuth endpoint etc. Basically used with "code" grant type
+     *
+     * @return Paysera_WalletApi_OAuth_Consumer
+     */
+    public function oauthConsumer()
+    {
+        return $this->container->createOAuthConsumer(
+            $this->clientId,
+            $this->oauthClient(),
+            $this->router
+        );
+    }
+
+    /**
+     * Creates callback handler, used when handling callbacks from Wallet API server
+     *
+     * @param Paysera_WalletApi_EventDispatcher_EventDispatcher $dispatcher
+     *
+     * @return Paysera_WalletApi_Callback_Handler
+     */
+    public function callbackHandler(Paysera_WalletApi_EventDispatcher_EventDispatcher $dispatcher)
+    {
+        return $this->container->createCallbackHandler($dispatcher, $this->router->getPublicKeyUri());
+    }
+
+    /**
+     * Creates OAuth client, responsible for getting access token.
+     * If "code" grant type is used, usually OAuth consumer is enough for all purposes, client itself is not needed
+     *
+     * @return Paysera_WalletApi_Client_OAuthClient
+     */
+    public function oauthClient()
+    {
+        return $this->container->createOAuthClient(
+            $this->basicClient($this->router->getOAuthApiEndpoint())
+        );
+    }
+
+    /**
+     * Creates basic client for making custom requests.
+     * Returned client is responsible for signing the requests etc., but no specific methods are defined
+     * and no mapping done.
+     *
+     * This could be used for functionality that is not yet implemented in specific client classes.
+     *
+     * @param string                                  $basePath
+     * @param Paysera_WalletApi_Entity_MacAccessToken $token
+     * @param array                                   $parameters project_id, location_id or some other parameters
+     *
+     * @return Paysera_WalletApi_Client_BasicClient
+     */
+    public function basicClient(
+        $basePath = null,
+        Paysera_WalletApi_Entity_MacAccessToken $token = null,
+        array $parameters = array()
+    ) {
+        return $this->container->createBasicClient($this->dispatcher($basePath, $token, $parameters));
+    }
+
+    /**
+     * Returns router, related to API. Can be used to get transaction confirmation URI
+     *
+     * @return Paysera_WalletApi_Util_Router
+     */
+    public function router()
+    {
+        return $this->router;
+    }
+
+    /**
+     * @param null                                    $basePath
+     * @param Paysera_WalletApi_Entity_MacAccessToken $token
+     * @param array                                   $parameters
+     *
+     * @return Paysera_WalletApi_EventDispatcher_EventDispatcher
+     */
+    protected function dispatcher(
+        $basePath = null,
+        Paysera_WalletApi_Entity_MacAccessToken $token = null,
+        array $parameters = array()
+    ) {
+        $basePath = $this->router->getApiEndpoint($basePath);
+        if ($token === null) {
+            $requestSigner = $this->container->createRequestSigner($this->signer);
+        } else {
+            $requestSigner = $this->container->createOAuthRequestSigner($this->oauthClient(), $token);
+        }
+
+        return $this->container->createDispatcherForClient(
+            $basePath,
+            $requestSigner,
+            $parameters
+        );
+    }
+}
---
+++ data/iq/src/Paysera/WalletApi/Container.php
@@ -0,0 +1,253 @@
+<?php
+
+/**
+ * Service and parameter container class.
+ * Only creates custom services, used in this API. Contains default creation login inside.
+ * Creates services only when needed, not on initialization
+ */
+class Paysera_WalletApi_Container
+{
+    /**
+     * @var Paysera_WalletApi_Mapper
+     */
+    protected $mapper;
+
+    /**
+     * @var Paysera_WalletApi_Http_ClientInterface
+     */
+    protected $webClient;
+
+    /**
+     * @var Paysera_WalletApi_EventDispatcher_EventDispatcher
+     */
+    protected $eventDispatcher;
+
+    /**
+     * For customizing service. In normal cases this should not be called
+     *
+     * @param Paysera_WalletApi_Mapper $mapper
+     *
+     * @return self for fluent interface
+     */
+    public function setMapper(Paysera_WalletApi_Mapper $mapper)
+    {
+        $this->mapper = $mapper;
+        return $this;
+    }
+
+    /**
+     * For customizing service. In normal cases this should not be called
+     *
+     * @param Paysera_WalletApi_Http_ClientInterface $webClient
+     *
+     * @return self for fluent interface
+     */
+    public function setWebClient(Paysera_WalletApi_Http_ClientInterface $webClient)
+    {
+        $this->webClient = $webClient;
+        return $this;
+    }
+
+    /**
+     * Sets eventDispatcher
+     *
+     * @param Paysera_WalletApi_EventDispatcher_EventDispatcher $eventDispatcher
+     *
+     * @return $this
+     */
+    public function setEventDispatcher($eventDispatcher)
+    {
+        $this->eventDispatcher = $eventDispatcher;
+        return $this;
+    }
+
+    /**
+     * @param string                                                     $basePath
+     * @param Paysera_WalletApi_EventDispatcher_EventSubscriberInterface $requestSigner
+     * @param array                                                      $parameters
+     *
+     * @return Paysera_WalletApi_EventDispatcher_EventDispatcher
+     */
+    public function createDispatcherForClient(
+        $basePath,
+        Paysera_WalletApi_EventDispatcher_EventSubscriberInterface $requestSigner,
+        array $parameters = array()
+    ) {
+        $dispatcher = new Paysera_WalletApi_EventDispatcher_EventDispatcher();
+        $dispatcher->mergeDispatcher($this->getEventDispatcher());
+        $dispatcher->addSubscriber(new Paysera_WalletApi_Listener_EndpointSetter($basePath));
+
+        if (count($parameters) > 0) {
+            $dispatcher->addSubscriber(new Paysera_WalletApi_Listener_ParameterSetter($parameters));
+        }
+
+        $dispatcher->addSubscriber($requestSigner);
+        $dispatcher->addSubscriber($this->createInvalidResponseListener());
+        return $dispatcher;
+    }
+
+    /**
+     * @param Paysera_WalletApi_EventDispatcher_EventDispatcher $dispatcher
+     *
+     * @return Paysera_WalletApi_Client_BasicClient
+     */
+    public function createBasicClient(Paysera_WalletApi_EventDispatcher_EventDispatcher $dispatcher)
+    {
+        return new Paysera_WalletApi_Client_BasicClient($this->getWebClient(), $dispatcher);
+    }
+
+    /**
+     * @param Paysera_WalletApi_Client_BasicClient $basicClient
+     *
+     * @return Paysera_WalletApi_Client_OAuthClient
+     */
+    public function createOAuthClient(Paysera_WalletApi_Client_BasicClient $basicClient)
+    {
+        return new Paysera_WalletApi_Client_OAuthClient($basicClient, $this->getMapper());
+    }
+
+    /**
+     * @param Paysera_WalletApi_Client_BasicClient $basicClient
+     *
+     * @return Paysera_WalletApi_Client_WalletClient
+     */
+    public function createWalletClient(Paysera_WalletApi_Client_BasicClient $basicClient)
+    {
+        return new Paysera_WalletApi_Client_WalletClient($basicClient, $this->getMapper());
+    }
+
+    /**
+     * @param Paysera_WalletApi_Client_BasicClient $basicClient
+     *
+     * @return Paysera_WalletApi_Client_TokenRelatedWalletClient
+     */
+    public function createWalletClientWithToken(Paysera_WalletApi_Client_BasicClient $basicClient)
+    {
+        return new Paysera_WalletApi_Client_TokenRelatedWalletClient($basicClient, $this->getMapper());
+    }
+
+    /**
+     * @param Paysera_WalletApi_Auth_SignerInterface $signer
+     *
+     * @return Paysera_WalletApi_Listener_RequestSigner
+     */
+    public function createRequestSigner(Paysera_WalletApi_Auth_SignerInterface $signer)
+    {
+        return new Paysera_WalletApi_Listener_RequestSigner($signer);
+    }
+
+    /**
+     * @return Paysera_WalletApi_Listener_InvalidResponseListener
+     */
+    public function createInvalidResponseListener()
+    {
+        return new Paysera_WalletApi_Listener_InvalidResponseListener();
+    }
+
+    /**
+     * @param Paysera_WalletApi_Client_OAuthClient          $client
+     * @param Paysera_WalletApi_Entity_MacAccessToken $token
+     *
+     * @return Paysera_WalletApi_Listener_OAuthRequestSigner
+     */
+    public function createOAuthRequestSigner(
+        Paysera_WalletApi_Client_OAuthClient $client,
+        Paysera_WalletApi_Entity_MacAccessToken $token
+    ) {
+        return new Paysera_WalletApi_Listener_OAuthRequestSigner($client, $token);
+    }
+
+    /**
+     * Creates OAuth consumer service
+     *
+     * @param string                               $clientId
+     * @param Paysera_WalletApi_Client_OAuthClient $oauthClient
+     * @param Paysera_WalletApi_Util_Router        $router
+     *
+     * @return Paysera_WalletApi_OAuth_Consumer
+     */
+    public function createOAuthConsumer($clientId, Paysera_WalletApi_Client_OAuthClient $oauthClient, Paysera_WalletApi_Util_Router $router)
+    {
+        return new Paysera_WalletApi_OAuth_Consumer(
+            $clientId,
+            $oauthClient,
+            $router,
+            new Paysera_WalletApi_State_SessionStatePersister('Paysera_WalletApi_' . $clientId),
+            new Paysera_WalletApi_Util_RequestInfo($_SERVER)
+        );
+    }
+
+    /**
+     * Gets service. Creates with default configuration if not yet available
+     *
+     * @param string                                          $clientId
+     * @param string|Paysera_WalletApi_Http_ClientCertificate $authentication string for MAC secret
+     *
+     * @return Paysera_WalletApi_Auth_SignerInterface
+     */
+    public function createAuthSigner($clientId, $authentication)
+    {
+        if ($authentication instanceof Paysera_WalletApi_Http_ClientCertificate) {
+            return new Paysera_WalletApi_Auth_ClientCertificate($authentication);
+        } else {
+            return new Paysera_WalletApi_Auth_Mac($clientId, $authentication);
+        }
+    }
+
+    /**
+     * Gets service. Creates with default configuration if not yet available
+     *
+     * @param Paysera_WalletApi_EventDispatcher_EventDispatcher $dispatcher
+     * @param string                                            $publicKeyUri
+     *
+     * @return Paysera_WalletApi_Callback_Handler
+     */
+    public function createCallbackHandler(Paysera_WalletApi_EventDispatcher_EventDispatcher $dispatcher, $publicKeyUri)
+    {
+        return new Paysera_WalletApi_Callback_Handler(
+            $dispatcher,
+            new Paysera_WalletApi_Callback_SignChecker($publicKeyUri, $this->getWebClient()),
+            $this->getMapper()
+        );
+    }
+
+    /**
+     * Gets service. Creates with default configuration if not yet available
+     *
+     * @return Paysera_WalletApi_EventDispatcher_EventDispatcher
+     */
+    public function getEventDispatcher()
+    {
+        if ($this->eventDispatcher === null) {
+            $this->eventDispatcher = new Paysera_WalletApi_EventDispatcher_EventDispatcher();
+        }
+        return $this->eventDispatcher;
+    }
+
+    /**
+     * Gets service. Creates with default configuration if not yet available
+     *
+     * @return Paysera_WalletApi_Mapper
+     */
+    public function getMapper()
+    {
+        if ($this->mapper === null) {
+            $this->mapper = new Paysera_WalletApi_Mapper();
+        }
+        return $this->mapper;
+    }
+
+    /**
+     * Gets service. Creates with default configuration if not yet available
+     *
+     * @return Paysera_WalletApi_Http_ClientInterface
+     */
+    protected function getWebClient()
+    {
+        if ($this->webClient === null) {
+            $this->webClient = new Paysera_WalletApi_Http_CurlClient();
+        }
+        return $this->webClient;
+    }
+
+}
